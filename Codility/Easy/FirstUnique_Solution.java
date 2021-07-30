81a1ab5cf96760757b9bfbc85d51f90ebfb55573
unknown <harrelsonjordan@gmail.com>
2021-07-30 11:14:45 -0400

    updated solution to be more efficient

--- Codility/Easy/FirstUnique_Solution.java
+++ Codility/Easy/FirstUnique_Solution.java
@@ -1,18 +1,26 @@
 // you can also use imports, for example:
 import java.util.*;
 
+//changed solution that resulted in 100%
+//use map instead of list and iterate at worst case O(2N) 
+//average time complexity O(N*log(N))
+//space is O(N) for the hashmap
+
 class Solution {
     public int solution(int[] A) {
-        List<Integer> theList = new ArrayList<Integer>();   //create a list to store numbers we've come across
-        for(int num : A)                                    //go through each num in the array
-        {                                                   //
-            if(theList.isEmpty())                           //if the list is empty
-                theList.add(num);                           //add the element
-            else if(!theList.remove(Integer.valueOf(num)))  //if the integer wasn't removed
-                theList.add(num);                           //add it
-        }                                                   //
-        if(theList.size() == 0)                             //if the list is empty
-            return -1;                                      //return -1
-        return theList.get(0);                              //otherwise return the first element
-    }                                                       //
-}                                                           //
+        HashMap<Integer,Integer> theMap = new HashMap<Integer,Integer>(); //create a map to store ints and how many times they occur
+        for(int num : A)                                                  //iterate through the array
+        {                                                                 //
+            if(theMap.containsKey(num))                                   //if the map contains the key
+                theMap.put(num, theMap.get(num)+1);                       //update it +1
+            else                                                          //otherwise
+                theMap.put(num,1);                                        //put it in the map with 1 occurance
+        }                                                                 //
+        for(int num : A)                                                  //iterate through the list again
+        {                                                                 //
+            if(theMap.get(num) == 1)                                      //find an element with one occurance? 
+                return num;                                               //return it
+        }                                                                 //
+        return -1;                                                        //return -1 one if no unique
+    }
+}
