6411c652b56fa309bd9b44467631b95cb433fd97
unknown <harrelsonjordan@gmail.com>
2021-07-30 21:08:19 -0400

    add july 30th solution constructor provided

---
+++ LeetCode/DailyChallenges/July30th__Solution.java
@@ -0,0 +1,21 @@
+class MapSum {                                                  //class mapping provided
+    /** Initialize your data structure here. */
+    public MapSum() {                                           //default constructor
+
+    }
+    
+    public void insert(String key, int val) {                   //insert method
+
+    }
+    
+    public int sum(String prefix) {                             //sum method of all strings in the map that start with the prefix
+
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
